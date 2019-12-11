d53a54348f259f3ddae7a328e5710b3c91168f56
Rashika-Katiyar <rashikakatiyar@gmail.com>
2019-12-11 19:30:41 +0530

    Added boilerplater app file for flask

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 01:50:37 2019
+
+@author: Rashika
+"""
+
+import FetchData
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def result():
+    
+      re=FetchData.data()
+      
+      return render_template("result.html")
+  
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+    
