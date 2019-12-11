b45645d97a96c010aeb6a2d74a8c658d4bacb063
Rashika-Katiyar <rashikakatiyar@gmail.com>
2019-12-11 20:04:24 +0530

    Final Commit

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def result():
     
       re=FetchData.data()
       
-      return render_template("result.html")
+      return render_template("result.html",tables= [re.to_html()])
   
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
